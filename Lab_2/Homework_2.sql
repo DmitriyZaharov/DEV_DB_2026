8f11b6a5e85cb8d0c26813f4fac9ca5d3de071c6
Dmitriy Zaharov <asparabus@ya.ru>
2026-03-01 23:46:38 +0300

    Update Homework_2.sql

--- Lab_2/Homework_2.sql
+++ Lab_2/Homework_2.sql
@@ -1,5 +1,5 @@
 --		Задание. Использование подзапросов
---
+
 --    1. Выведите информацию о заказах клиента, который был зарегистрирован в БД последним.
 --	Внешний запрос выбирает все столбцы из таблицы заказов только для этого конкретного custid
 SELECT 
@@ -7,39 +7,50 @@ SELECT
     orderid, 
     orderdate
 FROM "Sales"."Orders"
--- Сначала выполняется часть в скобках SELECT MAX(custid) 
--- она находит самый большой идентификатор клиента в таблице заказчиков:
 WHERE custid = (
     SELECT MAX(custid)
     FROM "Sales"."Customers"
 );
 
---    2. Выведите следующие данные по клиентам, которые сделали заказ в самую последнюю дату
---
-select
-	companyname,
-	contactname,
-	contacttitle, 
-	address,
-	city,
-	region,
-	postalcode 
-FROM "Sales"."Customers"
-WHERE orderdate = (
-    SELECT MAX(orderdate)-1
+SELECT 
+    o.custid, 
+    o.orderid, 
+    o.orderdate
+FROM "Sales"."Orders" as o
+-- JOIN:соединил таблицы Customers и Orders по общему полю custid
+JOIN "Sales"."Customers" AS c ON c.custid = o.custid
+WHERE o.custid = (
+    SELECT MAX(custid)
     FROM "Sales"."Customers"
+);
+
+--    2. Выведите следующие данные по клиентам, которые сделали заказ в самую последнюю дату
+SELECT 
+    c.companyname,
+    c.contactname,
+    c.contacttitle, 
+    c.address,
+    c.city,
+    c.region,
+    c.postalcode
+FROM "Sales"."Customers" AS c
+-- JOIN:соединил таблицы Customers и Orders по общему полю custid
+JOIN "Sales"."Orders" AS o ON c.custid = o.custid
+WHERE o.orderdate = (
+    SELECT MAX(orderdate) 
+    FROM "Sales"."Orders"
 );
 
 --    3. Выведите список клиентов, которые не делали заказов
 SELECT 
-	custid, 
-	companyname, 
-	contactname
-FROM "Sales"."Customers"
-WHERE custid NOT IN (
-    SELECT DISTINCT custid 
-    FROM "Sales"."Orders"
-    WHERE custid IS NOT NULL
+    c.custid, 
+    c.companyname, 
+    c.contactname
+FROM "Sales"."Customers" AS c
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM "Sales"."Orders" AS o 
+    WHERE o.custid = c.custid
 );
 
 --    4. Выведите список заказов тех клиентов, которые проживают в Mexico
@@ -55,20 +66,31 @@ WHERE custid IN (
     WHERE country = 'Mexico'
 );
 
+SELECT 
+    o.custid, 
+    o.orderid, 
+    o.orderdate, 
+    o.shipcountry 
+FROM "Sales"."Orders" AS o
+JOIN "Sales"."Customers" AS c ON o.custid = c.custid
+WHERE c.country = 'Mexico';
+
 --    5. Выведите самые дорогие продукты в каждой категории. Детали должны присутствовать!
 SELECT 
-	P1.productid , 
-	P1.productname ,
-	P1.supplierid , 
-	P1.categoryid , 
-	P1.unitprice ,
-	P1.discontinued 
-FROM "Production"."Products" AS P1
-WHERE unitprice = (
-    SELECT MAX(unitprice)
-    FROM "Production"."Products" AS P2
-    WHERE P2.categoryid = P1.categoryid
-);
+	p.productid , 
+	p.productname ,
+	p.supplierid , 
+	p.categoryid , 
+	p.unitprice ,
+	p.discontinued 
+FROM "Production"."Products" AS p
+WHERE p.unitprice = (
+-- Подзапрос находит макс. цену для категории текущей строки внешнего запроса
+    SELECT MAX(p2.unitprice)
+    FROM "Production"."Products" AS p2
+    WHERE p2.categoryid = p.categoryid
+)
+ORDER BY p.categoryid;
 
 
 
